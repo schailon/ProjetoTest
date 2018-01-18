7588720109176312021539a0ce535dd4797beec3
Schailon Beckmann <schailon.kiraly@iatec.com>
2018-01-18 17:47:26 -0200

    enviando projeto de teste criado

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+export class App {
+  configureRouter(config, router) {
+    config.title = 'Contacts';
+    config.map([
+      { route: '', moduleId: 'no-selection', title: 'Select' },
+      { route: 'contacts/:id', moduleId: 'contact-detail', name: 'contacts' }
+    ]);
+
+    this.router = router;
+  }
+}
---
+++ src/no-selection.ts
@@ -0,0 +1,6 @@
+export class NoSelection {
+    constructor() {
+
+        this.message = "Por favor selecione um contato.";
+    }
+}
